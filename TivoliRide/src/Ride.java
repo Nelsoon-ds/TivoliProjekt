9cb5544e0c95f1b75672538ab085365e168d4339
Nelsoon-ds <davidgaming108@gmail.com>
2025-09-03 11:35:58 +0200

    Første commit - Jeg tænker vi godt kan deale med merge conflicts etc

---
+++ TivoliRide/src/Ride.java
@@ -0,0 +1,39 @@
+package Lesson04.Exercise;
+
+import java.util.Scanner;
+
+public class Ride {
+    static long minHeight;
+    String name;
+
+
+    public Ride(long minHeight, String name) {
+        this.minHeight = minHeight;
+        this.name = name;
+    }
+
+    public String showName() {
+        return this.name;
+    }
+
+    public static double getHeight() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your height in centimeters?: ");
+        return sc.nextDouble();
+    }
+    public static boolean heightCheck() {
+        boolean tallEnough;
+        tallEnough = getHeight() > minHeight;
+        return tallEnough;
+    }
+    public static void startRide() {
+        if (heightCheck()) {
+            System.out.println("You're tall enough");
+        }  else {
+            System.out.println("Too small sonny boy");
+        }
+    }
+
+
+
+}
---
+++ TivoliRide/src/TivoliRideTest.java
@@ -0,0 +1,23 @@
+public class TivoliRideTest {
+    public static void main(String[] args) {
+        makeRide();
+    }
+    // method:
+    public void chooseRide() {
+        System.out.println("Choose a ride to go on!");
+        System.out.println("Available rides: ");
+    }
+
+    public static String[] makeRide() {
+        Lesson04.Exercise.Ride r1 = new Lesson04.Exercise.Ride(130, "Tower of Terror");
+        Lesson04.Exercise.Ride r2 = new Lesson04.Exercise.Ride(130, "Lego Racer");
+        Lesson04.Exercise.Ride r3 = new Lesson04.Exercise.Ride(130, "Red Devil");
+        String[] rides = {r1.toString(), r2.toString(), r3.toString()};
+
+        return rides;
+
+    }
+    // method:
+    public void printResult() {
+    }
+}
